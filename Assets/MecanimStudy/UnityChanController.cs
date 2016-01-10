f68a256682ffa27be5dc950cfcee707a60fa1115
syo-sa1982 <fujii1308@gmail.com>
2016-01-10 21:08:41 +0900

    歩かせた.

---
+++ Assets/MecanimStudy/UnityChanController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+	
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey ("up")) {
+			anim.SetBool ("Walk", true);
+		} else {
+			anim.SetBool ("Walk", false);
+		}
+	}
+}
