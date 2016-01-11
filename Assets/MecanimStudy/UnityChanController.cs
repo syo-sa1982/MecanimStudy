67256d0eeaf699797619f4e6ee7393402471ba26
syo-sa1982 <fujii1308@gmail.com>
2016-01-11 20:31:06 +0900

    歩かせたけど、コレジャナイ感

--- Assets/MecanimStudy/UnityChanController.cs
+++ Assets/MecanimStudy/UnityChanController.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UnityChanController : MonoBehaviour {
+public class UnityChanController : MonoBehaviour 
+{
 	
 	private Animator anim;
+	public Vector3 targetDirection;
 
 	// Use this for initialization
 	void Start () 
@@ -14,8 +16,13 @@ public class UnityChanController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKey ("up")) {
+		targetDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		
+		if (targetDirection.magnitude > 0.1) {
 			anim.SetBool ("Walk", true);
+			transform.rotation = Quaternion.LookRotation(targetDirection);
+			CharacterController conroller = GetComponent<CharacterController>();
+			conroller.Move(transform.forward * Time.deltaTime * 3f);
 		} else {
 			anim.SetBool ("Walk", false);
 		}
